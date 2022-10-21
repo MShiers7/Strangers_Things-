cb35fc993cf78dc5138085ecb3a87412a69ed3e4
MShiers7 <martyshiers7@gmail.com>
2022-10-21 16:39:13 -0500

    Rendered posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,11 +1,13 @@
 import React from "react";
+import PostsItem from "./PostsItem";
 
 const Posts = ({ posts }) => {
+  console.log("posts", posts);
   return (
     <div>
       <h1>Posts</h1>
-      {posts.map((post) => {
-        return <div>{post.title}</div>;
+      {posts.map((item) => {
+        return <PostsItem key={item._id} posts={item} />;
       })}
     </div>
   );
---
+++ src/components/PostsItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const PostsItem = ({ posts }) => {
+  return (
+    <div className="ui card">
+      <div className="content">
+        <div className="centered aligned header">{posts.author.username}</div>
+        <div className="centered aligned description">
+          {posts.title}
+          <p>{posts.description}</p>
+          <div className="center aligned header">{posts.price}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostsItem;
